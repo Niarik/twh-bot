f9924db1f7374d4d501bed4ec9bd12acb5f8563b
Niarik <30569924+Niarik@users.noreply.github.com>
2025-04-11 23:43:14 +0100

    Create water_manager.py

---
+++ water_manager.py
@@ -0,0 +1,8 @@
+#water_manager.py
+class WaterManager:
+    def __init__(self):
+        pass
+
+    async def start_water_loop(self):
+        # Every 3 hours check current season and apply water updates
+        pass
